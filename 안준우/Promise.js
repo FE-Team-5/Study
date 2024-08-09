3902dfcc943bfdbf55150de5c364b5935d162f85
JW-Ahn0 <jun.woo.an0@gmail.com>
2024-08-09 15:54:25 +0900

    [Feat] Promise 리뉴얼

--- 안준우/Promise.js
+++ 안준우/Promise.js
@@ -3,26 +3,92 @@ class AjwPromise {
   static FULFILLED = "FULFILLED";
   static REJECT = "REJECT";
 
+  #catchCallbacks = [];
+  #thenCallbacks = [];
   constructor(executor) {
     this.state = AjwPromise.PENDING;
     this.value = undefined;
     const resolve = (value) => {
-      if (this.state === AjwPromise.PENDING) {
-        this.value = value;
-        this.state = AjwPromise.FULFILLED;
-      }
+      this.update(AjwPromise.FULFILLED, value);
     };
     const reject = (value) => {
-      if (this.state === AjwPromise.PENDING) {
-        this.value = value;
-        this.state = AjwPromise.REJECT;
-      }
+      this.update(AjwPromise.REJECT, value);
     };
     try {
       executor(resolve, reject);
     } catch (error) {
       reject(error);
     }
+  }
+  #runCallbacks() {
+    if (this.state === AjwPromise.FULFILLED) {
+      this.#thenCallbacks.forEach((callback) => callback(this.value));
+      this.#thenCallbacks = [];
+    }
+
+    if (this.state === AjwPromise.REJECT) {
+      this.#catchCallbacks.forEach((callback) => callback(this.value));
+      this.#catchCallbacks = [];
+    }
+  }
+
+  update(state, value) {
+    queueMicrotask(() => {
+      if (this.state !== AjwPromise.PENDING) return;
+      if (value instanceof AjwPromise) {
+        value.then(resolve, reject);
+        return;
+      }
+      this.state = state;
+      this.value = value;
+      if (this.state === AjwPromise.REJECT) console.error(value);
+      this.#runCallbacks();
+    });
+  }
+  then(thenCallback, catchCallback) {
+    return new AjwPromise((resolve, reject) => {
+      this.#thenCallbacks.push((value) => {
+        if (!thenCallback) {
+          resolve(value);
+          return;
+        }
+
+        try {
+          resolve(thenCallback(value));
+        } catch (error) {
+          reject(error);
+        }
+      });
+
+      this.#catchCallbacks.push((value) => {
+        if (!catchCallback) {
+          reject(value);
+          return;
+        }
+
+        try {
+          resolve(catchCallback(value));
+        } catch (error) {
+          reject(error);
+        }
+      });
+    });
+  }
+  catch(catchCallback) {
+    return this.then(undefined, catchCallback);
+  }
+
+  finally(callback) {
+    return this.then(
+      (value) => {
+        callback();
+        return value;
+      },
+      (value) => {
+        callback();
+        throw value;
+      }
+    );
   }
 }
 
@@ -36,21 +102,42 @@ console.log(a);
 */
 
 /*
-2. Prmoise는 기본적으로 내부에 function(resolve,reject){} 모양으로 콜백을 전달함
-
+//Test 1. resolve, reject 잘 구현했는지 확인
+const b = new AjwPromise(function (resolve, reject) {
+  const value = null;
+  value.length = 1;
+  resolve(value);
+});
 */
 
-// const a = new Promise(function (resolve, reject) {
-//   const value = "aa";
-//   console.log(value);
-//   resolve(value);
+// const a = new Promise((resolve, reject) => {
+//   const a = null;
+//   a.length = 5;
+//   resolve("aa");
 // });
 
-// a.then((value) => {
-//   console.log("원본");
-//   console.log(value);
+// a.then(() => {
+//   console.log("then");
+// }).catch(() => {
+//   console.log("catch");
 // });
-const b = new AjwPromise(function (resolve, reject) {
-  const value = "bb";
-  reject(value);
+
+// 테스트 예제
+const asyncTest = new AjwPromise((resolve, reject) => {
+  resolve("Async Test Passed");
 });
+
+asyncTest
+  .then((result) => {
+    console.log("22 then");
+    console.log(result); // "Async Test Passed" 출력
+  })
+  .then((result) => {
+    console.log("11 then");
+  })
+  .catch((error) => {
+    console.log("캐치!");
+  })
+  .finally(() => {
+    console.log("파이ㅓㄹ");
+  });
