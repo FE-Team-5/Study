f07db6d0ae31bc068fc2740f9f5b32b774d2deb0
JW-Ahn0 <jun.woo.an0@gmail.com>
2024-09-02 01:08:06 +0900

    [Feat] 추가 및 삭제 기능 추가

--- 안준우/TodoList_ts/dist/index.js
+++ 안준우/TodoList_ts/dist/index.js
@@ -1,10 +1,16 @@
 "use strict";
 {
+    const $input = document.getElementById("input");
     const $todoList = document.getElementById("to-do-list");
+    const $addBtn = document.getElementById("input-add-btn");
     let list = [];
+    function init() {
+        $addBtn.addEventListener("click", onClickAdd);
+    }
     function render() {
-        list.forEach((item) => {
-            $todoList.innerHTML += makeListItem(item);
+        $todoList.innerHTML = "";
+        list.forEach((item, index) => {
+            $todoList.innerHTML += makeListItem(item, index);
         });
     }
     function getListFromLocalStorage() {
@@ -15,13 +21,35 @@
             render();
         }
     }
-    function makeListItem(item) {
+    function onClickAdd() {
+        const temp = {
+            text: $input.value,
+            done: false,
+        };
+        list.push(temp);
+        localStorage.setItem("todos", JSON.stringify(list));
+        render();
+    }
+    $todoList.addEventListener("click", (event) => {
+        const target = event.target;
+        if (target.id === "delete-btn") {
+            const li = target.closest("li");
+            const index = Number(li.getAttribute("data-index"));
+            if (index !== null) {
+                list.splice(index, 1);
+                localStorage.setItem("todos", JSON.stringify(list));
+                render();
+            }
+        }
+    });
+    function makeListItem(item, index) {
         return `
-    <li>
+    <li data-index=${index}>
           <span>${item.text}</span>
           <button id="delete-btn">Del</button>
     </li>
     `;
     }
+    init();
     getListFromLocalStorage();
 }
--- 안준우/TodoList_ts/src/index.ts
+++ 안준우/TodoList_ts/src/index.ts
@@ -1,14 +1,25 @@
 {
-  //const $input = document.getElementById("input")! as HTMLInputElement;
+  const $input = document.getElementById("input")! as HTMLInputElement;
   const $todoList = document.getElementById("to-do-list")! as HTMLInputElement;
+  const $addBtn = document.getElementById(
+    "input-add-btn"
+  )! as HTMLButtonElement;
   let list: Item[] = [];
-  type Item = {
+  //   type Item = {
+  //     text: string;
+  //     done: boolean;
+  //   };
+  interface Item {
     text: string;
     done: boolean;
-  };
+  }
+  function init(): void {
+    $addBtn.addEventListener("click", onClickAdd);
+  }
   function render(): void {
-    list.forEach((item: Item) => {
-      $todoList.innerHTML += makeListItem(item);
+    $todoList.innerHTML = "";
+    list.forEach((item: Item, index: number) => {
+      $todoList.innerHTML += makeListItem(item, index);
     });
   }
   function getListFromLocalStorage(): void {
@@ -20,14 +31,38 @@
     }
   }
 
-  function makeListItem(item: Item): string {
+  function onClickAdd(): void {
+    const temp: Item = {
+      text: $input.value,
+      done: false,
+    };
+    list.push(temp);
+    localStorage.setItem("todos", JSON.stringify(list));
+    render();
+  }
+
+  // 이벤트 전파 방식
+  $todoList.addEventListener("click", (event: MouseEvent) => {
+    const target: HTMLElement = event.target as HTMLElement;
+    if (target.id === "delete-btn") {
+      const li = target.closest("li"); // Find the closest <li> element
+      const index = Number(li!.getAttribute("data-index")); // Get the data-index attribute
+      if (index !== null) {
+        list.splice(index, 1);
+        localStorage.setItem("todos", JSON.stringify(list));
+        render();
+      }
+    }
+  });
+
+  function makeListItem(item: Item, index: number): string {
     return `
-    <li>
+    <li data-index=${index}>
           <span>${item.text}</span>
           <button id="delete-btn">Del</button>
     </li>
     `;
   }
-
+  init();
   getListFromLocalStorage();
 }
