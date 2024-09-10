456c75b159bef13de2f56a9cc3d992082822b9ca
nxnaxx <nxnaxx125@gmail.com>
2024-09-10 09:16:12 +0900

    feat: Add copy-to-clipboard feature for shortened URLs

--- 김도연/week4_url-shortening/src/components/molecules/ShortenLinkItem.tsx
+++ 김도연/week4_url-shortening/src/components/molecules/ShortenLinkItem.tsx
@@ -30,11 +30,26 @@ const ShortenLink = styled.p`
 export default function ShortenLinkItem({ originLink, shortenLink }: ShortenLinkItemProps) {
   const [isCopied, setIsCopied] = useState(false);
 
+  const copyShortenUrl = async (url: string) => {
+    if (isCopied) return;
+    try {
+      await navigator.clipboard.writeText(url);
+      setIsCopied((isCopied) => !isCopied);
+      alert('클립보드에 단축 링크를 복사했습니다.');
+    } catch (err) {
+      console.error('링크 복사에 실패했습니다:', err);
+    }
+  };
+
   return (
     <ShortenItemContainer>
       <OriginLink>{originLink}</OriginLink>
       <ShortenLink>{shortenLink}</ShortenLink>
-      <SquareFillButton size="small" primary={!isCopied}>
+      <SquareFillButton
+        size="small"
+        primary={!isCopied}
+        onClick={() => copyShortenUrl(shortenLink)}
+      >
         {isCopied ? 'Copied!' : 'Copy'}
       </SquareFillButton>
     </ShortenItemContainer>
